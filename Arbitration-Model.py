c1a1add2d30394c40dcbb756aa64899a5f1540ee
Colten Appleby <colten.appleby@gmail.com>
2023-12-13 23:48:37 -0500

    Added columns for positions. removed pitchers. * Random Forrest still not differentiating well

--- Arbitration-Model.py
+++ Arbitration-Model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from matplotlib.ticker import FuncFormatter
+from tabulate import tabulate
 
 from Models import BagLearner as bl, RTLearner as rt
 import math
@@ -18,14 +19,19 @@ def preprocess():
 
 	# data.dropna(subset=['G', 'Year Salary'], inplace=True)
 	# data = data.loc[~((data['Season'] < 2024) & (data['Next Year Salary'].isna()))]
-	data.drop(['Next Year Salary', 'Salary', 'SeasonStat'], axis=1, inplace=True)
+	data.drop(['Salary Diff', 'Salary', 'SeasonStat'], axis=1, inplace=True)
+
+	# %%
+	dummies = pd.get_dummies(data['Position'], prefix='pos')
+	data = pd.concat([data, dummies], axis=1)
+	data.drop(['Position'], axis=1, inplace=True)
 
 	# identifier = data['Player']
 	data['Player'] = data.pop('Player')
 	# data['playerid'] = data.pop('playerid')
 	# data['Season'] = data.pop('Season')
 
-	stat_to_predict = 'Salary Diff'
+	stat_to_predict = 'Next Year Salary'
 
 	x_train = data[data['Year'] < 2023].drop([stat_to_predict], axis=1).values
 	x_train = x_train[:, :-1]  # remove Player
@@ -48,49 +54,70 @@ def RandomForest():
 
 	# train the model
 	leaf_size = 5
-	bags = 20
-	runs = 50
 
-	#### BAG LEARNER ####
-	bagTrainRMSE = np.empty(runs, float)
-	bagTestRMSE = np.empty(runs, float)
+	#### Multiple Bag Learners ####
+	# bags = 20
+	# runs = 50
+	# bagTrainRMSE = np.empty(runs, float)
+	# bagTestRMSE = np.empty(runs, float)
+	# salary_2023 = np.empty((runs, y_test.shape[0]), float)
+	# salary_2024 = np.empty((runs, y_2024.shape[0]), float)
+	# for i in range(runs):
+	# 	learner = bl.BagLearner(learner=rt.RTLearner, kwargs={"leaf_size": leaf_size}, bags=bags, boost=False,
+	# 							verbose=False)
+	# 	learner.add_evidence(x_train, y_train)  # train it
+	# 	# Training Data
+	# 	pred_y = learner.query(x_train)
+	# 	TrainRmse = math.sqrt(((y_train - pred_y) ** 2).sum() / y_train.shape[0])  # TestRMSE
+	# 	bagTrainRMSE[i] = TrainRmse
+	# 	# Test
+	# 	pred_y = learner.query(x_test)
+	# 	salary_2023[i] = pred_y
+	# 	TestRmse = math.sqrt(((y_test - pred_y) ** 2).sum() / y_test.shape[0])  # TestRMSE
+	# 	bagTestRMSE[i] = TestRmse
+	# 	salary_2024[i] = learner.query(x_2024)
 
-	salary_2023 = np.empty((runs, y_test.shape[0]), float)
-	salary_2024 = np.empty((runs, y_2024.shape[0]), float)
+	bags = 100
+	learner = bl.BagLearner(learner=rt.RTLearner, kwargs={"leaf_size": leaf_size}, bags=bags, boost=False,
+							verbose=False)
 
-	for i in range(runs):
-		learner = bl.BagLearner(learner=rt.RTLearner, kwargs={"leaf_size": leaf_size}, bags=bags, boost=False,
-								verbose=False)
-		learner.add_evidence(x_train, y_train)  # train it
-		# Training Data
-		pred_y = learner.query(x_train)
-		TrainRmse = math.sqrt(((y_train - pred_y) ** 2).sum() / y_train.shape[0])  # TestRMSE
-		bagTrainRMSE[i] = TrainRmse
-		# Test
-		pred_y = learner.query(x_test)
-		salary_2023[i] = pred_y
-		TestRmse = math.sqrt(((y_test - pred_y) ** 2).sum() / y_test.shape[0])  # TestRMSE
-		bagTestRMSE[i] = TestRmse
-		salary_2024[i] = learner.query(x_2024)
+	learner.add_evidence(x_train, y_train)  # train it
+	# Training Data
+	y_pred = learner.query(x_train)
+	trainRmse = math.sqrt(((y_train - y_pred) ** 2).sum() / y_train.shape[0])  # TestRMSE
+
+	# Test
+	y_pred = learner.query(x_test)
+	testRmse = math.sqrt(((y_test - y_pred) ** 2).sum() / y_test.shape[0])  # TestRMSE
+
+	salary_2024 = learner.query(x_2024)
 
 	#### Save the Estimates ####
-	pred_y = np.mean(salary_2023, axis=0)
-	salary_2023_estimate = np.column_stack((test_names, y_test, pred_y))
-	df = pd.DataFrame(salary_2023_estimate)
+	# y_pred = np.mean(salary_2023, axis=0)
+	y_pred = y_pred.astype(int)
+	y_test = y_test.astype(int)
+	delta = y_test - y_pred
+	salary_2023_estimate = np.column_stack((test_names, y_test, y_pred, delta))
+	df = pd.DataFrame(salary_2023_estimate, columns=['Player', 'Actual', 'Predicted', "Difference"])
 	df.to_csv('./predictions/salary_2023_estimate.csv')
 
-	avg_diff_2023 = np.mean(np.abs(y_test - pred_y))
+	avg_diff_2023 = np.mean(np.abs(y_test - y_pred))
+
+	print("Salary 2023")
+	table = tabulate(df, headers='keys', tablefmt='pretty', showindex=False, intfmt=",")
+	print(table)
 	print(f"Average Difference 2023: {avg_diff_2023}")
+	print(f"Train RMSE: {trainRmse}")
+	print(f"Test RMSE: {testRmse}")
 
-	salary_2024_mean = np.mean(salary_2024, axis=0)
-	salary_2024_estimate = np.column_stack((names_2024, salary_2024_mean))
-	df = pd.DataFrame(salary_2024_estimate)
-	df.to_csv('./predictions/salary_2024_estimate.csv')
+	# salary_2024_mean = np.mean(salary_2024, axis=0)
+	salary_2024_estimate = np.column_stack((names_2024, salary_2024))
+	# df = pd.DataFrame(salary_2024_estimate)
+	# df.to_csv('./predictions/salary_2024_estimate.csv')
 
-	print(f"Test RMSE: {np.mean(bagTestRMSE)}")
-	print(f"Train RMSE: {np.mean(bagTrainRMSE)}")
 
-	residuals = y_test - pred_y
+
+	residuals = y_test - y_pred
 
 	# Create a histogram of residuals
 	plt.hist(y_test, bins=20, color='blue', alpha=0.7)
@@ -107,8 +134,8 @@ def RandomForest():
 
 	# plt.show()
 	# plt.show()
-	print(np.mean(y_test))
-	print(y_test)
+	# print(np.mean(y_test))
+	# print(y_test)
 
 
 def LinearRegressionModel():
@@ -127,8 +154,15 @@ def LinearRegressionModel():
 	r2 = r2_score(y_test, y_pred)
 
 	#### Save the Estimates ####
-	# salary_2023_mean = np.mean(y_pred, axis=0)
-	salary_2023_estimate = np.column_stack((test_names, y_test, y_pred))
+	pred_y = y_pred.astype(int)
+	y_test = y_test.astype(int)
+	delta = y_test - pred_y
+	salary_2023_estimate = np.column_stack((test_names, y_test, pred_y, delta))
+	df = pd.DataFrame(salary_2023_estimate, columns=['Player', 'Actual', 'Predicted', "Difference"])
+	print("Salary 2023")
+	table = tabulate(df, headers='keys', tablefmt='pretty', showindex=False, intfmt=",")
+	print(table)
+	# df.to_csv('./predictions/salary_2023_estimate.csv')
 	avg_diff_2023 = np.mean(np.abs(y_test - y_pred))
 	print(f"Average Difference 2023: {avg_diff_2023}")
 
@@ -142,4 +176,4 @@ def LinearRegressionModel():
 	# print(data.columns)
 
 if "__main__" == __name__:
-	RandomForest()
+	LinearRegressionModel()
